9b97e6ebd4937be65edadef1583a374ce3e8b9c7
hoseinalimoradi <ho.alimoradi@gmail.com>
2020-11-04 23:21:07 +0330

    Initial Commit

---
+++ nrgWayWellness/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nrgWayWellness
+//
+//  Created by Hosein Alimoradi on 8/14/1399 AP.
+//  Copyright © 1399 wellness. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
